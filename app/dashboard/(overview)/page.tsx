a7e140f39dc484f23737ca464d417851fed9220b
josem3129 <mar22069@byui.edu>
2024-11-19 16:26:05 -0700

    update 2.1

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,11 +1,13 @@
-// import { Card } from "../../ui/dashboard/cards";
-import RevenueChart from "../../ui/dashboard/revenue-chart";
-import LatestInvoices from "../../ui/dashboard/latest-invoices";
-import { lusitana } from "../../ui/fonts";
-import { fetchLatestInvoices , fetchCardData} from "../../lib/data";
-import { Suspense } from "react";
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
-import CardWrapper from "../../ui/dashboard/cards";
+import CardWrapper from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -13,7 +15,7 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   // const totalPaidInvoices = (await fetchCardData()).totalPaidInvoices;
   // const totalPendingInvoices = (await fetchCardData()).totalPaidInvoices;
   // const numberOfInvoices = (await fetchCardData()).numberOfInvoices;
