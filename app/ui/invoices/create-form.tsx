8b09c05bf8d7c6064f1715fcb561d94898d56651
josem3129 <mar22069@byui.edu>
2024-11-19 16:19:57 -0700

    update week 2 (2.0)

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,7 +11,7 @@ import {
 import { Button } from "@/app/ui/button";
 import { createInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
-import { error } from "console";
+// import { error } from "console";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
